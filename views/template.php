facfb9455ca6bbb5a8eb73f88b31d4efa85c5182
fernandoparada18 <fernandoparada18@openmailbox.org>
2016-02-07 00:19:29 -0430

    Commit inicial de la plantilla principal y las vistas de decentes #3

---
+++ views/template.php
@@ -0,0 +1,29 @@
+<?php namespace views;
+
+  class Template{
+
+    public function __construct(){
+?>
+  <!DOCTYPE html>
+  <html lang="es">
+    <head>
+      <meta charset="utf-8">
+      <title>..::PROGRAMA TODAS LAS MANOS A LA SIEMBRA::..</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="css/main.css">
+      <link rel="stylesheet" type="text/css" media="all" href="css/media.css">
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+      <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    </head>
+    <body>
+<?php
+    }
+
+    public function __destruct(){
+?>
+    </body>
+  </html>
+<?php
+    }
+  }
+?>
