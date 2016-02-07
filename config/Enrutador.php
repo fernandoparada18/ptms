38146512b57f1f2867af96c9d3172f2ee19b3689
fernandoparada18 <fernandoparada18@openmailbox.org>
2016-02-06 23:18:27 -0430

    Correcion de errores en la carga de los controladores #1

--- config/Enrutador.php
+++ config/Enrutador.php
@@ -2,8 +2,24 @@
 
   class Enrutador{
 
-    public static function run(Request $recuest){
-      $controlador = $recuest->getControlador() . "Controller";
+    public static function run(Request $request){
+      $controlador = $request->getControlador() . "Controller";
+      $ruta = ROOT . "controllers" . DS . $controlador . ".php";
+      $metodo = $request->getMetodo();
+      if($metodo == "index.php"){
+        $metodo = "index";
+      }
+      $argumento = $request->getArgumento();
+      if(is_readable($ruta)){
+        require_once $ruta;
+        $mostrar = "controllers\\" . $controlador;
+        $controlador = new $mostrar;
+        if(!isset($argumento)){
+          call_user_func(array($controlador, $metodo));
+        }else{
+          call_user_func_array(array($controlador, $metodo), $argumento);
+        }
+      }
     }
   }
 ?>
--- config/Request.php
+++ config/Request.php
@@ -11,15 +11,21 @@
         $ruta = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL);
         $ruta = explode("/", $ruta);
         $ruta = array_filter($ruta);
+        if($ruta[0] == "index.php"){
+          $this->controlador = "docente";
+        }else{
+          $this->controlador = strtolower(array_shift($ruta));
+        }
 
-        $this->controlador = strtolower(array_shift($ruta));
         $this->metodo = strtolower(array_shift($ruta));
 
         if(!$this->metodo){
           $this->metodo = "index";
         }
         $this->argumento = $ruta;
-        print $this->metodo;
+      }else{
+        $this->controlador = "docente";
+        $this->metodo = "index";
       }
     }
 
