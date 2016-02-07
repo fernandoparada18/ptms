6021a6ba2f2ac5970b8de53a0c62324ac7d9dc3c
fernandoparada18 <fernandoparada18@openmailbox.org>
2016-02-07 01:07:13 -0430

    Renderizado de la plantilla #3

--- views/template.php
+++ views/template.php
@@ -1,5 +1,7 @@
 <?php namespace views;
 
+  $template = new Template();
+
   class Template{
 
     public function __construct(){
@@ -10,17 +12,29 @@
       <meta charset="utf-8">
       <title>..::PROGRAMA TODAS LAS MANOS A LA SIEMBRA::..</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" type="text/css" href="css/main.css">
-      <link rel="stylesheet" type="text/css" media="all" href="css/media.css">
-      <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-      <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>views/template/css/main.css">
+      <link rel="stylesheet" type="text/css" media="all" href="<?php echo URL; ?>views/template/css/media.css">
+      <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>views/template/css/bootstrap.css">
+      <link rel="stylesheet" href="<?php echo URL; ?>views/template/font-awesome/css/font-awesome.min.css">
     </head>
     <body>
+      <div class="container">
+       <div class="row">
+         <div class="col-md-4">
+           <img src="<?php echo URL; ?>views/template/img/logozec.png" width="100%"></img>
+         </div>
+         <div class="col-md-4"></div>
+         <div class="col-md-4">
+           <img class="logoptms img-rounded" src="<?php echo URL; ?>views/template/img/logoptms.jpg" width="98%"></img>
+         </div>
+       </div>
+       <hr class="linea-horizontal"></hr>
 <?php
     }
 
     public function __destruct(){
 ?>
+      </div>
     </body>
   </html>
 <?php
